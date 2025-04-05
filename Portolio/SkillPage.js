86f1314c0c1d5660392c2a2869b585f4306257b9
Shreyash-17-K <shreyash10.2006@gmail.com>
2025-04-05 19:12:13 +0530

    Portolio page completed, blogging page added not edited (just nav footer and somethinf basic). and some more changes i dont remember.

--- Portolio/SkillPage.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const skills = document.querySelectorAll('.skill-level');
-    skills.forEach(skill => {
-        const level = skill.getAttribute('data-level');
-        skill.style.width = level; // Trigger animation
-    });
-});
