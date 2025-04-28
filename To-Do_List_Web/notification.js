cb9e4cb229afe72ce2d68a5e4923343c02a5f181
Shreyash-17-K <shreyash10.2006@gmail.com>
2025-04-28 14:35:08 +0530

    The exam is done and I'm going to make cchanges and make some of the best full fleged website. I'm committing beecause i don't want to forget what i did and remember and laugh.

--- To-Do_List_Web/notification.js
+++
@@ -1,9 +0,0 @@
-function checkDueDates() {
-    tasks.forEach(task => {
-        if (task.dueDate && new Date(task.dueDate) - new Date() < 86400000 && !task.completed) {
-            alert(`Reminder: Task "${task.name}" is due soon!`);
-        }
-    });
-}
-
-setInterval(checkDueDates, 3600000); // Check every hour
